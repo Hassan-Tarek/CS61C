c4c8632197efbb9cef95639ee51420cd6e3033ab
Hassan Tarek <hassanelashry415@gmail.com>
2023-05-01 00:19:06 +0100

    lab01 finished

--- labs/lab01/ex10_ll_cycle.c
+++ labs/lab01/ex10_ll_cycle.c
@@ -2,5 +2,17 @@
 #include "ex10_ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    if(head == NULL || head->next == NULL)
+        return 0;
+    node* fast_ptr = head->next->next;
+    node* slow_ptr = head;
+    while(fast_ptr != slow_ptr) {
+        slow_ptr = slow_ptr->next;
+        if(fast_ptr == NULL || fast_ptr->next == NULL)
+            return 0;
+        else
+            fast_ptr = fast_ptr->next->next;
+    }
+    // the list is cyclic
+    return 1;
 }
